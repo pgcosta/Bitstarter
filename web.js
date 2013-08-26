142f645df861cf4a3c8e60fa5d7c2797a0e42ccb
Pedro <pedro@Xpto.(none)>
2013-08-26 12:40:36 +0100

    hello world from index.html

--- web.js
+++ web.js
@@ -4,10 +4,12 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  index=fs.readFile("index.html")
-  response.send(index);
+  index=fs.readFileSync("index.html")
+  response.send(index.toString());
 });
 
+//x = expr1 || expr2
+//x = expr1 if expr1=true else = expr2
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
